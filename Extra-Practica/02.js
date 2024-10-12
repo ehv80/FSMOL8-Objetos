e12f7ba6f3177d6187100517343099fd96e2095a
ehv80 <ezequiel.villanueva@gmail.com>
2024-10-12 11:15:34 -0300

    Modificación de 02.js de la Práctica Extra

--- Extra-Practica/02.js
+++ Extra-Practica/02.js
@@ -5,13 +5,23 @@
   No comentar la funcion 
 */
 function stringMasLarga(strings) {
-    // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
-    // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
-    // Ej:
-    // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
-    // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
+  // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
+  // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
+  // Ej:
+  // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
+  // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
-    // Tu código aca
+  // Tu código aca
+
+  var masLarga = strings[0];
+
+  for (var i = 1; i < strings.lenght; i++) {
+    if (masLarga.lenght <= strings[i].length) {
+      masLarga = strings[i];
+    }
+  }
+
+  return masLarga;
 }
 
 // No modifiques nada debajo de esta linea //
