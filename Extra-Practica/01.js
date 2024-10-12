050e718e83b6070f4527cbc73f6105b3865082e1
ehv80 <ezequiel.villanueva@gmail.com>
2024-10-12 10:53:01 -0300

    Modificación de 01.js de la Práctica Extra

--- Extra-Practica/01.js
+++ Extra-Practica/01.js
@@ -5,12 +5,22 @@
   No comentar la funcion 
 */
 function soloNumeros(array) {
-    // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
-    // Debe devolver un arreglo con solo los enteros.
-    // Ej: 
-    // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
+  // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
+  // Debe devolver un arreglo con solo los enteros.
+  // Ej: 
+  // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
-    // Tu código aca:
+  // Tu código aca:
+
+  var arrayResultante = [];
+
+  for (var i = 0; i < array.lenght; i++) {
+    if (typeof array[i] === "number") {
+      arrayResultante.push(array[i]);
+    }
+  }
+
+  return arrayResultante;
 
 }
 
