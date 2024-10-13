6da823dfa41d9703e83706d817fae11f677cc530
ehv80 <ezequiel.villanueva@gmail.com>
2024-10-13 10:16:51 -0300

    Modificación de 04.js de la Práctica Extra

--- Extra-Practica/04.js
+++ Extra-Practica/04.js
@@ -6,14 +6,25 @@
 */
 
 function numeroSimetrico(num) {
-    // La funcion llamada 'numeroSimetrico' recibe como argumento un numero entero 'num'
-    // Esta devuelve true o false dependiendo de si el número es simétrico o no. 
-    // Un número es simétrico cuando es igual a su reverso.
-    // Ej:
-    // numeroSimetrico(11711) devuelve true
+  // La funcion llamada 'numeroSimetrico' recibe como argumento un numero entero 'num'
+  // Esta devuelve true o false dependiendo de si el número es simétrico o no. 
+  // Un número es simétrico cuando es igual a su reverso.
+  // Ej:
+  // numeroSimetrico(11711) devuelve true
 
-    // Tu código:
+  // Tu código:
 
+  var numString = num.toString();
+
+  var longitud = numString.length;
+
+  for (var i = 0; i < Math.floor((longitud / 2)); i++) {
+    if (numString[i] !== numString[(longitud - 1 - i)]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 // No modifiques nada debajo de esta linea //
