63dab716812842601622e0d66145f02b461f8f8a
ehv80 <ezequiel.villanueva@gmail.com>
2024-10-13 10:59:05 -0300

    Modificación de 06-07-08.js de la Práctica Extra

--- Extra-Practica/06-07-08.js
+++ Extra-Practica/06-07-08.js
@@ -12,6 +12,11 @@ function crearClasePersona() {
 
             // Tu código aca:
 
+            this.nombre = nombre;
+            this.edad = edad;
+            this.hobbies = hobbies;
+            this.amigos = amigos;
+
         }
 
         addFriend(nombre, edad) {
@@ -21,6 +26,9 @@ function crearClasePersona() {
 
             // Tu código aca:
 
+            var amigo = { nombre: nombre, edad: edad };
+            this.amigos.push(amigo);
+
         }
 
         addHobby(hobby) {
@@ -28,6 +36,8 @@ function crearClasePersona() {
             // No debe retornar nada.
 
             // Tu código aca:
+
+            this.hobbies.push(hobby);
 
         }
         getFriends() {
@@ -39,6 +49,14 @@ function crearClasePersona() {
 
             // Tu código aca:
 
+            var nombresAmigos = [];
+
+            for (var i = 0; i < this.amigos.length; i++) {
+                nombresAmigos.push(this.amigos[i].nombre);
+            }
+
+            return nombresAmigos;
+
         }
 
         getHobbies() {
@@ -47,6 +65,8 @@ function crearClasePersona() {
             // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
 
             // Tu código aca:
+
+            return this.hobbies;
 
         }
 
@@ -66,6 +86,14 @@ function crearClasePersona() {
             // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
 
             // Tu código aca:
+
+            var totalEdades = 0;
+
+            for (var i = 0; i < this.amigos.length; i++) {
+                totalEdades = totalEdades + this.amigos[i].edad;
+            }
+
+            return totalEdades / this.amigos.length;
 
         }
     };
