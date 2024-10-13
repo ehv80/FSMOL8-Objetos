6f9c0a72efd437fc9da6eff71b100bffeed4dfe6
ehv80 <ezequiel.villanueva@gmail.com>
2024-10-13 11:13:34 -0300

    Modificación de 09.js de la Práctica Extra

--- Extra-Practica/09.js
+++ Extra-Practica/09.js
@@ -5,25 +5,37 @@
   No comentar la funcion 
 */
 function filtrar(funcion) {
-    // Escribi una función filtrar en el prototipo de Arrays,
-    // que recibe una funcion (callback) que devuelve true o false.
-    // filtrar los elementos de ese arreglo en base al resultado de esa funcion
-    // comparadora, devolver un nuevo arreglo con los elementos filtrados.
-    // NO USAR LA FUNCION FILTER DE LOS ARREGLOS.
-    // ej:
-    // var productos = [{
-    //   price: 100,
-    //   name: 'tv'
-    // }, {
-    //   price: 50,
-    //   name: 'phone'
-    // }, {
-    //   price: 30,
-    //   name: 'lamp'
-    // }]
-    // productos.filtrar(function(p) {
-    //   return p.price >= 50;
-    // }) => [{price: 100, name:'tv'}]
+  // Escribi una función filtrar en el prototipo de Arrays,
+  // que recibe una funcion (callback) que devuelve true o false.
+  // filtrar los elementos de ese arreglo en base al resultado de esa funcion
+  // comparadora, devolver un nuevo arreglo con los elementos filtrados.
+  // NO USAR LA FUNCION FILTER DE LOS ARREGLOS.
+  // ej:
+  // var productos = [{
+  //   price: 100,
+  //   name: 'tv'
+  // }, {
+  //   price: 50,
+  //   name: 'phone'
+  // }, {
+  //   price: 30,
+  //   name: 'lamp'
+  // }]
+  // productos.filtrar(function(p) {
+  //   return p.price >= 50;
+  // }) => [{price: 100, name:'tv'}]
+
+  Array.prototype.filtrar = function (funcion) {
+    var filtrados = [];
+
+    for (var i = 0; i < this.length; i++) {
+      if (funcion(this[i])) {
+        filtrados.push(this[i]);
+      }
+    }
+
+    return filtrados;
+  };
 
 };
 
